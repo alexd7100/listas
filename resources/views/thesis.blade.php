11de50d48f625d46157bc3d874345a7b93002eb4
EDISSON ALEXANDER SUAZA BOTERO <N117-SISTEMAS@SEALCOSA.INTRANET>
2022-07-25 15:35:06 -0500

    CORS

--- resources/views/thesis.blade.php
+++ resources/views/thesis.blade.php
@@ -67,7 +67,6 @@
 
 <!-- Modal -->
 <form enctype="multipart/form-data" class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    @method('PUT')
     @csrf
     <div class="modal-dialog">
         <div class="modal-content">
